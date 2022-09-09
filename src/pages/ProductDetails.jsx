e258335d2a11da0ed8041d1e539c29a5f3d047ba
Rafael <bechstedt@hotmail.com>
2022-09-09 20:11:49 -0300

    Parte do requisito 09 - botão que add produto no local storage feito

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import { string, number, shape } from 'prop-types';
 
 export default class ProductDetails extends React.Component {
+  addInCart = () => {
+    const { selectedProduct } = this.props;
+    const cart = JSON.parse(localStorage.getItem('cart'));
+    const trueCart = (cart) || [];
+    trueCart.push(selectedProduct);
+    localStorage.setItem('cart', JSON.stringify(trueCart));
+  };
+
   render() {
     const { selectedProduct: { thumbnail, title, price } } = this.props;
 
@@ -10,6 +18,13 @@ export default class ProductDetails extends React.Component {
         <img src={ thumbnail } alt={ title } data-testid="product-detail-image" />
         <p data-testid="product-detail-name">{title}</p>
         <p data-testid="product-detail-price">{price}</p>
+        <button
+          type="button"
+          data-testid="product-detail-add-to-cart"
+          onClick={ this.addInCart }
+        >
+          Adicionar ao Carrinho
+        </button>
       </div>
     );
   }
