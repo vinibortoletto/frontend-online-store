cd8748216e9344d2dc57ac11151014999500ebbf
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-11-14 17:32:58 -0300

    refactor: SearchProducts component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import Categories from '../components/Categories';
 import { getProductsFromCategoryAndQuery } from '../services/api';
 import SearchProductsForm from '../components/SearchProducts';
+import ProductList from '../components/ProductList';
 
 class Home extends Component {
   state = {
@@ -18,8 +18,7 @@ class Home extends Component {
     });
   };
 
-  searchProducts = async (event, categoryId) => {
-    event.preventDefault();
+  searchProducts = async (categoryId) => {
     const { inputText } = this.state;
     this.setState(
       {
@@ -40,62 +39,6 @@ class Home extends Component {
     const { addToCart, getSelectedProduct } = this.props;
     const validation = dataProduct.length === 0;
 
-    const productList = (
-      <ul
-        className="grid sm:grid-cols-2 md:grid-cols-3
-      gap-4 justify-items-center"
-      >
-        {dataProduct.map((product) => (
-          <li
-            key={ product.id }
-            data-testid="product"
-            className="bg-white rounded-sm p-6 text-center relative"
-          >
-            <Link
-              to={ `/product-details/${product.id}` }
-              data-testid="product-detail-link"
-              onClick={ () => getSelectedProduct(product) }
-            >
-              <img
-                src={ product.thumbnail }
-                alt={ product.title }
-                className="w-52 mx-auto"
-              />
-              <h2 className="font-bold mb-2 truncate text-ellipsis max-w-[150px]">
-                {product.title}
-              </h2>
-              <h4 className="font-bold text-blue-600 text-lg">
-                R$
-                {product.price}
-              </h4>
-
-              {
-                product.shipping.free_shipping && (
-                  <h5
-                    className="w-16 rounded-sm bg-blue-600 text-white text-sm p-2
-                    absolute top-4 right-4"
-                    data-testid="free-shipping"
-                  >
-                    Frete Grátis
-                  </h5>
-                )
-              }
-            </Link>
-
-            <button
-              type="button"
-              onClick={ () => addToCart(product) }
-              data-testid="product-add-to-cart"
-              className="bg-teal-400 text-white font-bold px-2 py-4
-              rounded-sm mt-4 w-full"
-            >
-              <span>Adicionar ao carrinho</span>
-            </button>
-          </li>
-        ))}
-      </ul>
-    );
-
     return (
       <div className="lg:grid lg:grid-cols-2">
         <Categories searchProducts={ this.searchProducts } />
@@ -112,7 +55,13 @@ class Home extends Component {
 
             <div className="lg:p-10 lg:ml-64 flex justify-center">
               { !validation && !isLoading
-                ? (productList)
+                ? (
+                  <ProductList
+                    dataProduct={ dataProduct }
+                    getSelectedProduct={ getSelectedProduct }
+                    addToCart={ addToCart }
+                  />
+                )
                 : (
                   <p className="text-xl mt-10">Nenhum produto foi encontrado</p>
                 )}
