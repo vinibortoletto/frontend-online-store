30ce7ec1aa65f5d1abb200ee37beaa6448a7fc28
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-11-14 21:34:24 -0300

    style: FeedbackForm component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { shape, arrayOf, func } from 'prop-types';
 
+const ONE = 1;
+const TWO = 2;
+const THREE = 3;
+const FOUR = 4;
+const FIVE = 5;
+
 export default class FeedbackForm extends React.Component {
   state = {
     feedbackEmail: '',
@@ -59,86 +65,67 @@ export default class FeedbackForm extends React.Component {
       isValid,
     } = this.state;
 
+    const ratingFields = [ONE, TWO, THREE, FOUR, FIVE];
+
     return (
       <div>
-        <form onSubmit={ this.handleSubmit }>
-          <label htmlFor="feedbackEmail">
-            <input
-              type="email"
-              id="feedbackEmail"
-              name="feedbackEmail"
-              data-testid="product-detail-email"
+        <form
+          onSubmit={ this.handleSubmit }
+          className="my-10 md:max-w-3xl md:mx-auto"
+        >
+          <h3 className="font-bold text-xl text-center mb-4">Avaliações</h3>
+
+          <div className="grid gap-4">
+            <label htmlFor="feedbackEmail">
+              <span className="absolute -left-full">Email</span>
+              <input
+                type="email"
+                id="feedbackEmail"
+                name="feedbackEmail"
+                data-testid="product-detail-email"
+                onChange={ this.handleChange }
+                value={ feedbackEmail }
+                placeholder="Email"
+                className="border p-2 w-full mb-2"
+              />
+            </label>
+
+            <div className="flex gap-2 justify-center">
+              {ratingFields.map((field) => (
+                <label htmlFor={ field } key={ field }>
+                  <input
+                    type="radio"
+                    name="feedbackRating"
+                    id={ field }
+                    data-testid={ `${field}-rating` }
+                    onChange={ this.handleChange }
+                    className="mr-1"
+                  />
+                  {field}
+                </label>
+              ))}
+            </div>
+
+            <textarea
+              name="feedbackEvaluation"
+              id=""
+              cols="30"
+              rows="10"
+              data-testid="product-detail-evaluation"
               onChange={ this.handleChange }
-              value={ feedbackEmail }
+              value={ feedbackEvaluation }
+              className="border p-2 w-full mb-2"
+              placeholder="Comentários"
             />
-          </label>
 
-          <label htmlFor="1">
-            <input
-              type="radio"
-              name="feedbackRating"
-              id="1"
-              data-testid="1-rating"
-              onChange={ this.handleChange }
-            />
-            1
-          </label>
-
-          <label htmlFor="2">
-            <input
-              type="radio"
-              name="feedbackRating"
-              id="2"
-              data-testid="2-rating"
-              onChange={ this.handleChange }
-            />
-            2
-          </label>
-
-          <label htmlFor="3">
-            <input
-              type="radio"
-              name="feedbackRating"
-              id="3"
-              data-testid="3-rating"
-              onChange={ this.handleChange }
-            />
-            3
-          </label>
-
-          <label htmlFor="4">
-            <input
-              type="radio"
-              name="feedbackRating"
-              id="4"
-              data-testid="4-rating"
-              onChange={ this.handleChange }
-            />
-            4
-          </label>
-
-          <label htmlFor="5">
-            <input
-              type="radio"
-              name="feedbackRating"
-              id="5"
-              data-testid="5-rating"
-              onChange={ this.handleChange }
-            />
-            5
-          </label>
-
-          <textarea
-            name="feedbackEvaluation"
-            id=""
-            cols="30"
-            rows="10"
-            data-testid="product-detail-evaluation"
-            onChange={ this.handleChange }
-            value={ feedbackEvaluation }
-          />
-
-          <button type="submit" data-testid="submit-review-btn">Enviar</button>
+            <button
+              type="submit"
+              data-testid="submit-review-btn"
+              className="w-full bg-teal-400 text-slate-100 font-bold rounded p-2"
+            >
+              Enviar
+            </button>
+          </div>
         </form>
 
         {!isValid && (<p data-testid="error-msg">Campos inválidos</p>)}
