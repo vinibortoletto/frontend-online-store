5708d7bd6f8371c704123d66db5c3e76248c86b3
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-11-14 21:41:15 -0300

    style: Feedback component

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -5,17 +5,26 @@ export default class Feedback extends React.Component {
   render() {
     const { feedbacks } = this.props;
     return (
-      <div>
-        {
-          feedbacks
-            .map(({ email, text, rating }, index) => (
-              <div key={ index }>
-                <p data-testid="review-card-email">{email}</p>
-                <p data-testid="review-card-rating">{rating}</p>
-                <p data-testid="review-card-evaluation">{text}</p>
+      <div className="grid gap-4 md:max-w-3xl md:mx-auto">
+        {feedbacks
+          .map(({ email, text, rating }, index) => (
+            <div
+              key={ index }
+              className="bg-white shadow py-4 px-10 text-left"
+            >
+              <div className="flex gap-4">
+                <p
+                  data-testid="review-card-email"
+                  className="font-bold"
+                >
+                  {email}
+                </p>
+                <p data-testid="review-card-rating">{`Avaliação: ${rating}/5`}</p>
               </div>
-            ))
-        }
+
+              <p data-testid="review-card-evaluation">{text}</p>
+            </div>
+          ))}
       </div>
     );
   }
